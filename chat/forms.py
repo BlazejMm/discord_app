f66fb796cfab6054901da7564433b00f9424dbea
izabreb808 <iza.magda.rebisz@gmail.com>
2026-05-02 16:46:26 +0200

    v2

--- chat/forms.py
+++ chat/forms.py
@@ -8,26 +8,35 @@ from .models import Channel
 class RegisterForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ("username", "email")
+        fields = ("username",)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.update({"class": "form-control"})
-        self.fields["email"].required = True
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self._build_email(user.username)
+        if commit:
+            user.save()
+        return user
+
+    def _build_email(self, username):
         User = get_user_model()
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Ten email jest juz zajety.")
+        base = username.lower().replace(" ", "")
+        email = f"{base}@komunikator.local"
+        counter = 1
+        while User.objects.filter(email=email).exists():
+            email = f"{base}{counter}@komunikator.local"
+            counter += 1
         return email
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ("email", "avatar_url", "bio")
+        fields = ("avatar_url", "bio")
         labels = {
             "avatar_url": "Link do avatara",
             "bio": "Opis",
@@ -40,16 +49,6 @@ class ProfileForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.update({"class": "form-control"})
-
-    def clean_email(self):
-        email = self.cleaned_data["email"]
-        User = get_user_model()
-        qs = User.objects.filter(email=email)
-        if self.instance.pk:
-            qs = qs.exclude(pk=self.instance.pk)
-        if qs.exists():
-            raise forms.ValidationError("Ten email jest juz zajety.")
-        return email
 
 
 class ChannelForm(forms.ModelForm):
